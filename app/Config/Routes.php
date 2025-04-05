56d62219f1921d61d46c513cad8c7e2b37497e3c
Bestin-SJ <thecoderbean@gmail.com>
2025-04-05 23:34:13 +0530

    working

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,12 +1,15 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+namespace Config;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'WorkRequestController::index');
-$routes->post('/work-request/submit', 'WorkRequestController::submit');
+$routes = service('routes');
+
+// Load the system's routing file first
+
+
+// Custom routes
+$routes->get('/', 'WorkRequestController::index'); // Front page
+$routes->post('/work-request/submit', 'WorkRequestController::submit'); // Work request submission
 $routes->group('admin', function($routes) {
     $routes->get('/', 'AdminController::index', ['as' => 'admin.login']);
     $routes->post('login', 'AdminController::login');
